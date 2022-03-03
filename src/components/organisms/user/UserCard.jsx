78b542e66a7eb1867163434f40d2d70ddc01fe63
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-03-03 12:06:53 +0900

    Create Organism

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>test@example.com</dd>
+        <dt>電話番号</dt>
+        <dd>000-0000-0000</dd>
+        <dt>会社名</dt>
+        <dd>Feel Good Inc.</dd>
+        <dt>web</dt>
+        <dd>example.com</dd>
+      </dl>
+    </div>
+  );
+};
