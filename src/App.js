ce045e5506eb7106e051ce09af5f463270ab6fcd
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-03-03 18:02:19 +0900

    Create pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "デンジ",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "test@example.com",
-  phone: "111-1111-1111",
-  company: {
-    name: "Feel Good Inc."
-  },
-  website: "https://example.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `ユーザー${val}号`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "test@example.com",
+    phone: "111-1111-1111",
+    company: {
+      name: "Feel Good Inc."
+    },
+    website: "https://example.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SUserArea = styled.div`
+  padding: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
